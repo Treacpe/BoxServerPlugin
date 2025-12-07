70445422508125e8ee455441690515a50f283389
Treacpe <treacpe@gmail.com>
2025-12-07 07:02:48 -0600

    Update CustomItem.java
    Removed luck change of item dropping, Only for creation of item.

--- src/main/java/me.treacpe.boxServerPlugin/Items/CustomItem.java
+++ src/main/java/me.treacpe.boxServerPlugin/Items/CustomItem.java
@@ -5,37 +5,40 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class CustomItem {
+
     private final Material material;
     private final String name;
     private final String collectionCategory;
-    private final double dropChance;
 
-    public CustomItem(Material material, String name, String collectionCategory, double dropChance) {
+    public CustomItem(Material material, String name, String collectionCategory) {
         this.material = material;
         this.name = name;
         this.collectionCategory = collectionCategory;
-        this.dropChance = dropChance;
     }
 
     public Material getMaterial() {
         return material;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public String getCollectionCategory() {
         return collectionCategory;
     }
 
-    public double getDropChance() {
-        return dropChance;
-    }
-
-    public ItemStack getDrop() {
+    // Converts this CustomItem into an actual ItemStack you can give players
+    public ItemStack toItemStack() {
         ItemStack item = new ItemStack(material);
         ItemMeta meta = item.getItemMeta();
+
         if (meta != null) {
             meta.setDisplayName(name);
             item.setItemMeta(meta);
         }
+
         return item;
     }
 }
+
