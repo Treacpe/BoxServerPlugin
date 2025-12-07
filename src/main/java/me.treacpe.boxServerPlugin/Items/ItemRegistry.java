c0abff41dc8df24309834a4d5749905a17ebd2a1
Treacpe <treacpe@gmail.com>
2025-12-07 07:57:39 -0600

    Update ItemRegistry.java
    Replaced all with CreateItem and not customItem.

--- src/main/java/me.treacpe.boxServerPlugin/Items/ItemRegistry.java
+++ src/main/java/me.treacpe.boxServerPlugin/Items/ItemRegistry.java
@@ -6,14 +6,14 @@ import java.util.*;
 
 public class ItemRegistry {
 
-    public static final CustomItem 
-        Tier1_Wooden_Pickaxe = new CustomItem(
+    public static final CreateItem 
+        Tier1_Wooden_Pickaxe = new CreateItem(
             Material.WOODEN_PICKAXE,
             "&FTier 1&7 Wooden Pickaxe",
             "Mineral",
             True);
-    public static final CustomItem 
-        Tier2_Wooden_Pickaxe = new CustomItem(
+    public static final CreateItem 
+        Tier2_Wooden_Pickaxe = new CreateItem(
             Material.WOODEN_PICKAXE,
             "&FTier 2&7 Wooden Pickaxe",
             "Mineral",
