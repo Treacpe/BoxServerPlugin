7a25ef5145aa496682e18a8f071bec10f4b9a5b0
Treacpe <treacpe@gmail.com>
2025-12-07 09:38:47 -0600

    Update ItemRegistry.java

--- src/main/java/me.treacpe.boxServerPlugin/Items/ItemRegistry.java
+++ src/main/java/me.treacpe.boxServerPlugin/Items/ItemRegistry.java
@@ -1,42 +1,30 @@
 package me.treacpe.boxServerPlugin.items;
 
-import org.bukkit.Material;
-import org.bukkit.Enchantment;
-import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.*;
 
 public class ItemRegistry {
 
-    private static final Map<String, CreateItem> registry = new HashMap<>();
+    public static Map<String, ItemData> ITEMS = new HashMap<>();
 
-    public static void register(String id, CreateItem item) {
-        registry.put(id, item);
+    public static void load(JavaPlugin plugin) {
+        plugin.reloadConfig();
+        ItemLoader loader = new ItemLoader(plugin);
+        ITEMS = loader.loadItems();
     }
 
-    public static CreateItem get(String id) {
-        return registry.get(id);
+    public static void save(JavaPlugin plugin, ItemData data) {
+        ItemSaver saver = new ItemSaver(plugin);
+        saver.saveItem(data);
+    }
+
+    public static ItemData get(String id) {
+        return ITEMS.get(id);
     }
 
     public static boolean exists(String id) {
-        return registry.containsKey(id);
+        return ITEMS.containsKey(id);
     }
-
-    public static Map<String, CreateItem> getAll() {
-        return registry;
-    }
-
-    public static final CreateItem 
-        Tier1_Wooden_Pickaxe = new CreateItem(
-            Material.WOODEN_PICKAXE,
-            "&FTier 1&7 Wooden Pickaxe",
-            "Mineral",
-            true);
-    public static final CreateItem 
-        Tier2_Wooden_Pickaxe = new CreateItem(
-            Material.WOODEN_PICKAXE,
-            "&FTier 2&7 Wooden Pickaxe",
-            "Mineral",
-            true).addEnchantment(Enchantment.EFFICIENCY, 1);
 }
